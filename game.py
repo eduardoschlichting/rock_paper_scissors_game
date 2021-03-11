2f0e175bfbba5b959b95cfc45c1c203975b2136f
Eduardo Schlichting <schlichting.eduardo@gmail.com>
2021-03-10 19:33:42 -0500

    Building out initial game logic

---
+++ game.py
@@ -0,0 +1,51 @@
+# importing a package to extend python (just like we extended Sublime, Atom or VSCode)
+from random import randint
+
+# [] => this is an array
+# variable = [value1, value2, value3]
+# an array is a special tyoe of container that can hold multiple items
+# arrays are indexed (their contents are assigned a number)
+# the index always starts at 0
+choices = ["rock", "paper", "scissors"]
+
+# Version 1, to explain array indexing
+#player_choice = choices[1]
+#print("index 1 in the choice array is " + player_choice + ", which is paper")
+
+player_choice = input("Choose rock, paper, or scissors: ")
+
+print("User chose: " + player_choice)
+
+# This will be the AI choice -> a random pick from the choices array
+computer_choice = choices[randint (0, 2)]
+
+print("Computer chose: " + computer_choice)
+
+if computer_choice == player_choice:
+	print("tie!")
+
+elif computer_choice == "rock":
+	if player_choice == "scissors":
+		print("you lose!")
+	else:
+		print("you win!")
+
+elif computer_choice == "paper":
+	if player_choice == "scissors":
+		print("you win!")
+	else:
+		print("you lose!")
+
+elif computer_choice == "scissors":
+	if player_choice == "paper":
+		print("you lose!")
+	else:
+		print("you win!")
+*
+
+
+
+
+
+
+
